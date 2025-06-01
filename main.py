80dd0a2612dc6786fe3e1306577271973e4968f2
vguima404 <victorribeiro4004@gmail.com>
2025-06-01 14:23:00 -0300

    adicionado a lista de abrigos com endereco e seus respectivos valores de capacidade

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ zonas = [
     {"bairro": "Corrêas", "risco": "moderado"},
 ]
 
+abrigos = [
+    {"nome": "Pátio Petrópolis Shopping", "endereco": "Rua Marechal Deodoro, 153, Centro", "capacidade": 5000},
+    {"nome": "Escola Municipal Vereador José Fernandes da Silva", "endereco": "Rua Teresa, 1781, Alto da Serra", "capacidade": 300},
+    {"nome": "CIEP 472 Candido Portinari", "endereco": "Estr. União e Indústria, 11960, Itaipava", "capacidade": 1000},
+]
