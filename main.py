87ed8c39104bdb5ab3ccbf786d2af5800a6517e3
vguima404 <victorribeiro4004@gmail.com>
2025-06-01 14:09:54 -0300

    Adicionado a lista de zonas com os bairros de Petrópolis e seus respectivos graus de risco

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+zonas = [
+    {"bairro": "Centro", "risco": "alto"},
+    {"bairro": "Quitandinha", "risco": "moderado"},
+    {"bairro": "Itaipava", "risco": "baixo"},
+    {"bairro": "Alto da Serra", "risco": "alto"},
+    {"bairro": "Corrêas", "risco": "moderado"},
+]
+
